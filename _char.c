fcf78d94d930ccd005369de70f37dd3882904506
sai <ojiip95@gmail.com>
2023-06-25 19:17:24 +0200

    _char, handls the c specifier

---
+++ _char.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_char - function to print char
+ * @args: list of arguments from _printf function
+ * Return: 1 on seccuss
+ */
+
+int print_char(va_list args)
+{
+	int ch;
+
+	ch = va_arg(args, int);
+	_putchar(ch);
+
+	return (1);
+}
